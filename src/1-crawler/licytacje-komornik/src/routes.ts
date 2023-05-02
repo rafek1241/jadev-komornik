baf9a0cbb1ad6ef4c776f26a494ee4592a831df0
Robert Szachnowski <rafeq1241@gmail.com>
2023-05-02 18:43:04 +0200

    fix: prices and location details

--- src/1-crawler/licytacje-komornik/src/routes.ts
+++ src/1-crawler/licytacje-komornik/src/routes.ts
@@ -1,6 +1,8 @@
 import {Dataset, createPlaywrightRouter} from 'crawlee';
 
 export const router = createPlaywrightRouter();
+const pageDb = await Dataset.open('page');
+const detailDb = await Dataset.open('detail');
 
 // homepage
 router.addDefaultHandler(async ({page, enqueueLinks, log}) => {
@@ -15,8 +17,6 @@ router.addDefaultHandler(async ({page, enqueueLinks, log}) => {
     )
         .filter((link) => link != null);
 
-    log.info(`enqueueing new URLs: ['${links.join("', '")}']']`);
-
     await enqueueLinks({
         urls: <string[]>links,
         label: 'list',
@@ -25,13 +25,13 @@ router.addDefaultHandler(async ({page, enqueueLinks, log}) => {
 
 // page within specific filter
 router.addHandler('list', async ({page, parseWithCheerio, enqueueLinks, log}) => {
-    log.info(`loading page '${await page.title()}'`);
+    log.info(`loading page with main category '${await page.title()}'`);
     await page.waitForSelector('#main-content > div > div:nth-child(3)');
     const $ = await parseWithCheerio();
     let text = $('#main-content > div > div:nth-child(3)')
         .text().split(' ')
-            .filter(x => x.length > 0)
-            .map(x => x.replace('\n', ''))
+        .filter(x => x.length > 0)
+        .map(x => x.replace('\n', ''))
     ;
     const pageCount = parseInt(text.pop());
     const pages = [];
@@ -39,7 +39,6 @@ router.addHandler('list', async ({page, parseWithCheerio, enqueueLinks, log}) =>
         pages.push(`${await page.url()}?page=${i}`);
     }
 
-    log.info(`enqueueing new URLs: ['${pages.join("', '")}']`);
     await enqueueLinks({
         urls: pages,
         label: 'page',
@@ -50,7 +49,7 @@ router.addHandler('list', async ({page, parseWithCheerio, enqueueLinks, log}) =>
 router.addHandler('page', async ({page, parseWithCheerio, enqueueLinks, log}) => {
     const url = await page.url();
     log.info(`loading page '${url}'`);
-    
+
     await page.waitForSelector('#main-content > table');
     const $ = await parseWithCheerio();
 
@@ -61,18 +60,27 @@ router.addHandler('page', async ({page, parseWithCheerio, enqueueLinks, log}) =>
             const detailsLink = $(element).find('td:last-child > a').get()[0];
             const id = $(detailsLink).attr('href')?.split('/')?.pop();
             const url = $(detailsLink).prop('href');
+            const cityAndDistrict = $(element)
+                .find('td:nth-child(5)')
+                .text()
+                .split('\n')
+                .filter(x => x.trim().length > 0)
+                .map(x => x.trim().replace('/[\(\)]*/gm', ''));
             return {
                 id: id,
                 url: url,
                 hasPhoto: $(element).find('td:nth-child(2)').has('.photo').length > 0,
                 auctionDate: $(element).find('td:nth-child(3)').text().trim(),
                 name: $(element).find('td:nth-child(4) > div').text().trim(),
-                city: $(element).find('td:nth-child(5)').text().split('<br>').shift()?.trim(),
-                district: $(element).find('td:nth-child(5)').text().split('<br>').pop()?.trim()?.replace('/[\(\)]*/gm', ''),
-                initialPrice: (parseInt($(element).find('td:nth-child(6)').text().trim()
-                    .replace("zł", "")
-                    .replace(",", "")
-                    .replace("&nbsp;", "")) ?? 100) / 100,
+                city: cityAndDistrict[0],
+                district: cityAndDistrict[1],
+                prices: $(element)
+                        .find('td:nth-child(6)')
+                        .text()
+                        .split('\n')
+                        .filter(x => x.trim().length > 0)
+                        .map(x => x.replace(/[  ,(zł)]*/gm, ''))
+                        .map(x => parseInt(x ?? 100) / 100),
                 source: 'page'
             }
         })
@@ -81,8 +89,8 @@ router.addHandler('page', async ({page, parseWithCheerio, enqueueLinks, log}) =>
             item.url = url.split('.pl').shift() + '.pl' + item.url;
             return item;
         });
-    
-    await Dataset.pushData([...items]);
+
+    await pageDb.pushData([...items]);
 
     await enqueueLinks({
         urls: <string[]>items.map(x => x.url),
@@ -104,7 +112,7 @@ router.addHandler('detail', async ({page, parseWithCheerio, log}) => {
         .map((_, element) => $(element).attr('href'))
         .get()
         .map(link => url.split('.pl').shift() + '.pl' + link);
-    
+
     const item = {
         id: page.url().split('/').pop(),
         address: address,
@@ -112,6 +120,6 @@ router.addHandler('detail', async ({page, parseWithCheerio, log}) => {
         description: text,
         source: 'detail'
     };
-    
-    await Dataset.pushData(item);
+
+    await detailDb.pushData(item);
 });
